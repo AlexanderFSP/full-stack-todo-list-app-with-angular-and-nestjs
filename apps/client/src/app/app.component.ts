fdbb76cd401d5b8eef9f7c1e38880312f725cab5
Alexander <alexanderfsp@mail.ru>
2022-10-26 20:43:12 +0300

    Reuse todo list template from https://github.com/AlexanderFSP/todo-list-ethereum-dapp-with-ng

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,6 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { Message } from '@full-stack-todo-list-app-with-angular-and-nestjs/api-interfaces';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export interface ITaskView {
+  id: number;
+  content: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,6 +15,50 @@ import { Message } from '@full-stack-todo-list-app-with-angular-and-nestjs/api-i
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  @ViewChild(MatSelectionList) public selectionList!: MatSelectionList;
+
+  public tasks$: Observable<ITaskView[]>;
+
+  public readonly newTask = new FormControl<string>('');
+
+  private readonly _tasks$ = new BehaviorSubject<ITaskView[]>([]);
+
+  constructor() {
+    this.tasks$ = this._tasks$.asObservable();
+  }
+
+  public async onAddTask(): Promise<void> {
+    const task = this.newTask.value?.trim();
+
+    if (task?.length) {
+      try {
+        // TODO: Integrate with back
+
+        this.newTask.reset('');
+
+        // TODO: Update tasks
+        // eslint-disable-next-line no-empty
+      } catch {}
+    }
+  }
+
+  // TODO: option -> options
+  public async onSelectionChange({ options }: MatSelectionListChange): Promise<void> {
+    this.selectionList.setDisabledState(true);
+
+    console.log(options);
+
+    // TODO: Uncomment
+    // const task = option.value as ITaskView;
+
+    try {
+      // TODO: Integrate with back
+      // TODO: Update tasks
+    } catch {
+      // TODO: Revert selection
+      // option.toggle();
+    } finally {
+      this.selectionList.setDisabledState(false);
+    }
+  }
 }
--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
